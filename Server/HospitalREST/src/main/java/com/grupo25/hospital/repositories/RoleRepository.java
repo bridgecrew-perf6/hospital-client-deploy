ea41dc80ede7a21fdecbbccfbcc54d72c48234d3
ivettepinto7 <00186319@uca.edu.sv>
2022-06-22 16:49:40 -0600

    Clean directories

--- Server/HospitalREST/src/main/java/com/grupo25/hospital/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grupo25.hospital.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.grupo25.hospital.models.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
